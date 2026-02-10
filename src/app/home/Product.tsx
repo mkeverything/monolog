c6362b9cf33a2463512d38c8ada05774d861e678
Anatoly <yatskovanatoly@gmail.com>
2026-02-10 17:18:30 +0300

    Product section

--- src/app/home/Product.tsx
+++ src/app/home/Product.tsx
@@ -1,5 +1,5 @@
 import { FullPageSection } from '@/src/components/FullPageSection'
-import { Card, CardProps } from '@/src/components/ui/Card'
+import { Card } from '@/src/components/ui/Card'
 import { SiteContent } from '@/src/lib/cms'
 import Image from 'next/image'
 import { FC } from 'react'
@@ -7,6 +7,7 @@ import { FC } from 'react'
 export default function Product({ product }: SiteContent['home']) {
   return (
     <FullPageSection className='p-0!'>
+      <span className='flex h-32 items-center text-xl'>{product.title}</span>
       <div className='relative flex h-full items-center justify-center overflow-hidden'>
         <Image
           className='z-10 m-auto max-w-[75%]'
@@ -15,21 +16,33 @@ export default function Product({ product }: SiteContent['home']) {
           height={1178}
           alt='hero'
         />
+        <div className='absolute inset-0 z-10 bg-transparent bg-linear-to-b from-60% to-white to-75%' />
         <div className='border-primary absolute top-8 aspect-square h-auto w-[150%] rounded-full border-4 blur-lg' />
         <div className='border-primary absolute top-1/3 aspect-square h-auto w-full rounded-full border-4 blur-lg' />
-        <div className='absolute inset-0 bg-transparent bg-linear-to-b from-60% to-white to-75%' />
+        <FloatingCard card={product.cards[0]} coordinates='top-8 left-8' />
+        <FloatingCard card={product.cards[1]} coordinates='right-8 top-40' />
+        <FloatingCard card={product.cards[2]} coordinates='bottom-40 left-8' />
       </div>
     </FullPageSection>
   )
 }
 
-const FloatingCard: FC<FloatingCardProps> = ({ card, ...props }) => {
+const FloatingCard: FC<FloatingCardProps> = ({ card, coordinates }) => {
   return (
-    <Card variant='elevated' className='flex' {...props}>
-      <Image src={card.image} fill alt='card-image' />
-      <div className='flex flex-col'>
-        <span>{card.title}</span>
-        <span>{card.caption}</span>
+    <Card
+      variant='elevated'
+      className={`bg-base-100 absolute z-20 flex gap-2 rounded-2xl! p-2 ${coordinates}`}
+    >
+      <Image
+        src={card.image}
+        className='size-8'
+        width={1024}
+        height={1024}
+        alt='card-image'
+      />
+      <div className='flex flex-col text-xs'>
+        <span className=''>{card.title}</span>
+        <span className='text-secondary-content italic'>{card.caption}</span>
       </div>
     </Card>
   )
@@ -41,4 +54,5 @@ type FloatingCardProps = {
     title: string
     caption: string
   }
-} & CardProps
+  coordinates: string
+}
