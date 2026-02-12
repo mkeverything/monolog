ea961747acdcc58a24125b6e52e573e6e7106557
Anatoly <yatskovanatoly@gmail.com>
2026-02-12 07:37:42 +0300

    Projects page

--- src/app/projects/Hero.tsx
+++ src/app/projects/Hero.tsx
@@ -1,15 +1,44 @@
 'use client'
 
-import { FullPageSection } from '@/src/components/FullPageSection'
 import Image from 'next/image'
 import { ProjectsProps } from './page'
 
 export default function Hero(projects: ProjectsProps) {
   return (
-    <FullPageSection>
-      <div className='h-full w-full p-8'>
-        <Image src={projects.hero.image} fill alt='hero' />
+    <div className='flex h-[75dvh] flex-col p-2'>
+      <div className='size-full'>
+        <div className='relative h-1/2 w-full'>
+          <Image src={projects.hero.image} className='h-fit' fill alt='hero' />
+        </div>
+        <div className='grid w-full grid-cols-2 gap-8 md:grid-cols-5'>
+          <div className='flex flex-col md:col-span-3'>
+            <span className='text-secondary-content'>
+              {projects.hero.content.projects.title}
+            </span>
+            <span className='text-xl font-semibold'>
+              {projects.hero.content.projects.subtitle}
+            </span>
+          </div>
+
+          <div className='flex flex-col'>
+            <span className='text-secondary-content'>
+              {projects.hero.content.year.title}
+            </span>
+            <span className='text-xl font-semibold'>
+              {projects.hero.content.year.subtitle}
+            </span>
+          </div>
+
+          <div className='flex flex-col'>
+            <span className='text-secondary-content'>
+              {projects.hero.content.returned.title}
+            </span>
+            <span className='text-xl font-semibold'>
+              {projects.hero.content.returned.subtitle}
+            </span>
+          </div>
+        </div>
       </div>
-    </FullPageSection>
+    </div>
   )
 }
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,13 +1,17 @@
 import { getSiteContent, SiteContent } from '@/src/lib/cms'
 import Hero from './Hero'
 import Projects from './Projects'
+import DiscussCard from '@/src/components/DiscussCard'
 
 export default function Page() {
-  const { projects } = getSiteContent()
+  const { discuss, projects } = getSiteContent()
   return (
     <main className='p-4'>
       <Hero {...projects} />
       <Projects {...projects} />
+      <div className='my-32'>
+        <DiscussCard discuss={discuss} />
+      </div>
     </main>
   )
 }
