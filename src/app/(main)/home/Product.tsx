66c333462588cf4e324b20b27fdf8d53d489ebb5
Anatoly <yatskovanatoly@gmail.com>
2026-02-19 05:30:42 +0300

    Minor styling

--- src/app/(main)/home/Product.tsx
+++ src/app/(main)/home/Product.tsx
@@ -6,19 +6,20 @@ import { FC } from 'react'
 
 export default function Product({ product }: SiteContent['home']) {
   return (
-    <FullPageSection id='product' className='p-0!'>
+    <FullPageSection id='product' className='overflow-hidden'>
       <span className='flex h-32 items-center text-xl'>{product.title}</span>
-      <div className='relative flex min-h-full flex-1 items-center justify-center overflow-hidden'>
+      <div className='relative flex w-full items-center justify-center'>
         <Image
-          className='z-10 m-auto max-w-[75%]'
+          className='z-10 m-auto h-auto w-full max-w-4xl object-contain px-4'
           src={product.hero}
           width={1178}
           height={1178}
           alt='hero'
+          priority
         />
-        <div className='absolute inset-0 z-10 bg-transparent bg-linear-to-b from-60% to-white to-75%' />
-        <div className='border-primary absolute top-8 aspect-square h-auto w-[150%] rounded-full border-4 blur-lg' />
-        <div className='border-primary absolute top-1/3 aspect-square h-auto w-full rounded-full border-4 blur-lg' />
+        <div className='pointer-events-none absolute inset-0 z-10 m-auto w-1/2 bg-transparent bg-linear-to-b from-60% to-white to-85%' />
+        <div className='border-primary pointer-events-none absolute top-0 left-1/2 aspect-square h-auto w-[200vw] -translate-x-1/2 rounded-full border-4 blur-lg' />
+        <div className='border-primary pointer-events-none absolute top-1/3 left-1/2 aspect-square h-auto w-[120vw] -translate-x-1/2 rounded-full border-4 blur-lg' />
         <FloatingCard card={product.cards[0]} coordinates='top-8 left-8' />
         <FloatingCard card={product.cards[1]} coordinates='right-8 top-40' />
         <FloatingCard card={product.cards[2]} coordinates='bottom-40 left-8' />
