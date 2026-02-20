11ee3dbed39a5eaac17d5244c39646a9f4248166
Anatoly <yatskovanatoly@gmail.com>
2026-02-20 13:25:55 +0300

    Header layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,7 @@ export function Header() {
 
   return (
     <header className='fixed top-0 right-0 left-0 z-50 bg-transparent'>
-      <div className='mx-auto w-full max-w-7xl px-4 sm:px-6 lg:px-6 xl:px-6'>
+      <div className='w-full px-4 sm:px-6 lg:px-6 xl:px-6'>
         <div className='grid h-16 grid-cols-[1fr_auto_1fr] items-center sm:h-20'>
           <div className='flex items-center gap-4'>
             <Link
